6a694b35583c6ff7c2085377208499e86a93869c
toxa16 <toxa16@gmail.com>
2019-11-16 01:59:53 +0200

    rendering app version v0.0.1

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ function App() {
     <div className="App" data-cy="homepage">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>v0.0.1</p>
       </header>
     </div>
   );

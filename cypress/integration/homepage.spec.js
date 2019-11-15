3809613329d95316864bf4d7d1fcc1cffdcc29ce
toxa16 <toxa16@gmail.com>
2019-11-16 01:10:59 +0200

    init basic e2e test

---
+++ cypress/integration/homepage.spec.js
@@ -0,0 +1,8 @@
+describe('Homepage', () => {
+  it('should contain itself', () => {
+    // visit app dev URL
+    cy.visit('http://localhost:3000/');
+    // see the homepage element
+    cy.get('[data-cy="homepage"]');
+  });
+});

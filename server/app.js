2efbaf1b177116d85d033ebcf1b96b6cf1ac9efe
toxa16 <toxa16@gmail.com>
2019-11-29 02:53:51 +0200

    updating GET and POST /login tests

--- server/app.js
+++ server/app.js
@@ -1,24 +1,45 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(express.static('build'));
 
+/**
+ * GET /
+ */
 app.get('/', (req, res) => {
   res.sendFile('../build/index.html');
 });
 
+/**
+ * Stub (test) endpoint.
+ */
+app.get('/foobar', (req, res) => res.end('foobar'));
+
+/**
+ * GET /login
+ */
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, './login.html'));
+  const { username } = req.cookies;
+  if (username) {
+    res.redirect('/');
+  } else {
+    res.sendFile(path.join(__dirname, './login.html'));
+  }
 });
 
+/**
+ * POST /login
+ */
 app.post('/login', (req, res) => {
   const { username } = req.body;
-  console.log(username);
-  res.end('POST /login');
+  res.cookie('username', username);
+  res.redirect('/');
 });
 
 module.exports = app;
--- server/app.spec.js
+++ server/app.spec.js
@@ -6,6 +6,7 @@ describe('Production Server', () => {
   describe('GET /', () => {
     it.todo('should redirect to "/login" on unauthenticated user');
   });
+
   describe('GET /login', () => {
     it('should return 200 on unauthorized user', done => {
       supertest(app)
@@ -13,6 +14,29 @@ describe('Production Server', () => {
         .expect(200, done);
     });
 
-    it.todo('should redirect to "/" when the "username" cookie is set');
+    it('should redirect to "/" when the "username" cookie is set', done => {
+      supertest(app)
+        .get('/login')
+        .set('Cookie', 'username=charlie')
+        .expect(302)
+        .expect('Location', '/')
+        .end(done);
+    });
+  });
+
+  describe('POST /login', () => {
+    it(
+      'should redirect to "/" and set the "username" cookie' +
+        ' with the body "username" value',
+      done => {
+        supertest(app)
+          .post('/login')
+          .send(`username=charlie`)
+          .expect(302)
+          .expect('Location', '/')
+          .expect('Set-Cookie', /username=charlie/)
+          .end(done);
+      },
+    );
   });
 });

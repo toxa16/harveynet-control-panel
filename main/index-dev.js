2f89855b40af88762c0608edae2da8de46898cd0
toxa16 <toxa16@gmail.com>
2019-12-01 01:51:12 +0200

    main dev build infrastructure

---
+++ main/index-dev.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Main from './main';
+
+const root = document.getElementById('root');
+ReactDOM.render(<Main />, root);

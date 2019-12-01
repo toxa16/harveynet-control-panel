6cc89ef0ac85307c17bbff3965541dea062060ac
toxa16 <toxa16@gmail.com>
2019-12-01 03:05:04 +0200

    e2e session-control

---
+++ cypress/e2e/session-control.spec.js
@@ -0,0 +1,24 @@
+describe('Session-Control', () => {
+  it('should navigate from session machine list to control and back', () => {
+    // authenticate as user "alice"
+    cy.setCookie('username', 'alice');
+    // visit the app home path
+    cy.visit('/');
+
+    // see the "session" view with "machine list"
+    // click the "control link"
+    cy.get('[data-cy="session"]')
+      .find('[data-cy="machine-list"]')
+      .find('[data-cy="control-link"]')
+      .click();
+
+    // see the "control" view
+    // click the back ("session") link
+    cy.get('[data-cy="control"]')
+      .find('[data-cy="session-link"]')
+      .click();
+
+    // see the "machine list" again
+    cy.get('[data-cy="machine-list"]');
+  });
+});
--- session/session.js
+++ session/session.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Session() {
+  const [isControlOpen, setControlOpen] = useState(false);
+
+  function renderControl() {
+    return <div data-cy="control">
+      <h3>Control</h3>
+      <button
+        className="btn btn-primary"
+        data-cy="session-link"
+        onClick={ e => setControlOpen(false) }
+      >
+        Back To Session
+      </button>
+    </div>
+  }
+
+  function renderMachineList() {
+    return <div data-cy="machine-list">
+      <h3>Machine List</h3>
+      <button
+        className="btn btn-primary"
+        data-cy="control-link"
+        onClick={ e => setControlOpen(true) }
+      >
+        Open Control
+      </button>
+    </div>
+  }
+
+  function renderBody() {
+    return isControlOpen ? renderControl() : renderMachineList();
+  }
+
   return (
     <div data-cy="session">
-      <h1>Session</h1>
+      <h2>Session</h2>
+      
+      <div className="mt-5">
+        { renderBody() }
+      </div>
     </div>
   );
 }

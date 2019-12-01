6bff057e8392a19cdfd5f30451b25f85fba19b61
toxa16 <toxa16@gmail.com>
2019-12-01 03:08:49 +0200

    marking main integration tests as redundant

--- cypress/main/main.spec.js
+++ cypress/main/main.spec.js
@@ -1,4 +1,7 @@
-describe('Main', () => {
+/**
+ * THIS TEST SUITE IS POSSIBLY REDUNDANT.
+ */
+describe.skip('Main', () => {
   it('should open control and close control', () => {
     // visit homepath
     cy.visit('/');

7d45fe267c12c80783540b35d418122535079f6e
toxa16 <toxa16@gmail.com>
2020-05-07 22:08:11 +0300

    e2e real auth0 authentication

--- cypress/integration/panel.spec.js
+++ cypress/integration/panel.spec.js
@@ -1,10 +1,35 @@
+import auth0 from 'auth0';
+
+
+const domain = Cypress.env('AUTH0_DOMAIN');
+const clientId = Cypress.env('AUTH0_CLIENT_ID');
+const clientSecret = Cypress.env('AUTH0_CLIENT_SECRET');
+const realm = Cypress.env('AUTH0_REALM');
+
+const username = 'alice@email.com';
+const password = 'Alice1111$';
+
+
 describe('Panel scenario', () => {
-  beforeEach(() => {
+  let accessToken;
+
+  beforeEach(done => {
+    // loading fixtures
+    // TODO: simply `import` fixtures
     cy.fixture('alice-machines.json').as('expectedMachines');
+
+    // getting real access token
+    const authClient = new auth0.AuthenticationClient({
+      domain, clientId, clientSecret,
+    });
+    authClient.passwordGrant({ username, password, realm })
+      .then(authData => {
+        accessToken = authData.access_token;
+        done();
+      });
   });
 
   it('authenticate, render user machines', function () {
-    const accessToken = 'ACCESS_TOKEN_STUB';  // alice's token
     cy.visit(`/login#access_token=${accessToken}`); // simulating login
 
     const actualMachines = [];

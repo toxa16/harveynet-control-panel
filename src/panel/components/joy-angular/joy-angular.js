d5b19abc241540baa80ec36317079fa25e4b78b3
toxa16 <toxa16@gmail.com>
2020-06-28 23:09:41 +0300

    init reading joystick values

--- src/panel/components/joy-angular/joy-angular.js
+++ src/panel/components/joy-angular/joy-angular.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Joystick } from 'react-joystick-component';
 
 
 export default function JoyAngular() {
+  const [value, setValue] = useState(0);
+
+  function handleStart(e) {
+    console.log(e);
+  }
+  function handleStop(e) {
+    console.log(e);
+    setValue(0);
+  }
+  function handleMove(e) {
+    setValue(Math.round(e.x));
+  }
+
   return (
-    <div style={{
-      position: 'fixed',
-      right: '3rem',
-      bottom: '3rem',
-      textAlign: 'center',
-    }}>
-      <div className="mb-2">Angular</div>
+    <div style={{ position: 'fixed', right: '3rem', bottom: '3rem', textAlign: 'center' }}>
+      <div className="mb-2" style={{
+        display: 'inline-block',
+        padding: '.25rem .5rem',
+        background: 'rgba(255, 255, 255, .5)',
+      }}>
+        Angular: {value}
+      </div>
+
       <Joystick
         size={100}
         baseColor="red"
         stickColor="blue"
-        move={() => {}}
-        stop={() => {}}
+        start={handleStart}
+        move={handleMove}
+        stop={handleStop}
       ></Joystick>
     </div>
   );
--- src/panel/components/joy-linear/joy-linear.js
+++ src/panel/components/joy-linear/joy-linear.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Joystick } from 'react-joystick-component';
 
 
 export default function JoyLinear() {
+  const [value, setValue] = useState(0);
+
+  function handleStart(e) {
+    console.log(e);
+  }
+  function handleStop(e) {
+    console.log(e);
+    setValue(0);
+  }
+  function handleMove(e) {
+    setValue(Math.round(e.y));
+  }
+
   return (
-    <div style={{
-      position: 'fixed',
-      left: '3rem',
-      bottom: '3rem',
-      textAlign: 'center',
-    }}>
-      <div className="mb-2">Linear</div>
+    <div style={{ position: 'fixed', left: '3rem', bottom: '3rem', textAlign: 'center' }}>
+      <div className="mb-2" style={{
+        display: 'inline-block',
+        padding: '.25rem .5rem',
+        background: 'rgba(255, 255, 255, .5)',
+      }}>
+        Linear: {value}
+      </div>
+
       <Joystick
         size={100}
         baseColor="red"
         stickColor="blue"
-        move={() => {}}
-        stop={() => {}}
+        start={handleStart}
+        move={handleMove}
+        stop={handleStop}
       ></Joystick>
     </div>
   );
